a0f39b4aaeb77505016093016bbbb1ec73ac4c86
duswn158 <duswn157@naver.com>
2020-10-28 21:34:20 +0900

    연주 : 일주일치 확진자, 누적 확진자 json, pom.xml 자이썬, homecontroller 수정

--- src/main/java/com/corona/green/HomeController.java
+++ src/main/java/com/corona/green/HomeController.java
@@ -1,17 +1,15 @@
 package com.corona.green;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Handles requests for the application home page.
  */
 @Controller 
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	@RequestMapping(value = "main.do")
 	public String home(Model model) {
