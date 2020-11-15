c1f9bf0b1f3ef039a6fb7efe463d12838d983505
hyunmoSung <49181503+shm1113@users.noreply.github.com>
2020-11-15 16:47:54 +0900

    HyunMo : socket 재연결 에러수정

--- src/main/webapp/resources/js/green_socket.js
+++ src/main/webapp/resources/js/green_socket.js
@@ -1,20 +1,20 @@
 
-
 $(document).ready(function (){
 	 if(WebSocket.readyState!==0){
 			 connectWs();
 			 }
 });
 
+
 function connectWs(){
 	   
 	 sock = new WebSocket("wss://coronagreen.cf/socket.do");
-	 socket = sock; 
 	 
 
 
 //웹 소켓이 열리면 호출 
 	sock.onopen = function() {
+		console.log("시작해볼까")
  };
 
 //서버로부터 메시지를 받았을 때
@@ -26,7 +26,10 @@ sock.onmessage = function(evt) {
 };
 //서버와 연결을 끊었을때
 sock.onclose = function() {
-	 setTimeout(function(){conntectWs();} , 1000); 
+	console.log("끊겼다 다시간다~")
+	  setTimeout(function() {
+         connectWs();
+       }, 1000);
 };
 
 sock.onerror = function (err) {console.log('Errors : ' , err);};
