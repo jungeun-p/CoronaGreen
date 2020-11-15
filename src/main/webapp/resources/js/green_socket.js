b77f3f44f7aed517959e41703bde865ba43a8b8a
hyunmoSung <49181503+shm1113@users.noreply.github.com>
2020-11-15 17:41:08 +0900

    HyunMo : cosole.log 완전 삭제

--- src/main/webapp/resources/js/green_socket.js
+++ src/main/webapp/resources/js/green_socket.js
@@ -14,7 +14,6 @@ function connectWs(){
 
 //웹 소켓이 열리면 호출 
 	sock.onopen = function() {
-		console.log("시작해볼까")
  };
 
 //서버로부터 메시지를 받았을 때
@@ -26,7 +25,6 @@ sock.onmessage = function(evt) {
 };
 //서버와 연결을 끊었을때
 sock.onclose = function() {
-	console.log("끊겼다 다시간다~")
 	  setTimeout(function() {
          connectWs();
        }, 1000);
